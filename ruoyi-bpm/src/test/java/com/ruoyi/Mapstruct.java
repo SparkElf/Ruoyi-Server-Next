b7f366d43ee9b1eea8efe595db5585a6b59fd64d
SparkElf <374656045@qq.com>
2024-04-08 01:25:16 +0800

    解决test依赖

--- ruoyi-bpm/src/test/java/com/ruoyi/Mapstruct.java
+++ ruoyi-bpm/src/test/java/com/ruoyi/Mapstruct.java
@@ -2,9 +2,6 @@ package com.ruoyi;
 
 import io.github.linpeilie.Converter;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
